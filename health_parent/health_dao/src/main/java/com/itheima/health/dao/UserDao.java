32714d95f206592b99e8f07a1fcd3c2572989ca2
TALON-WAT <talonwat@gmeil.com>
2019-12-12 16:11:18 +0800

    左侧菜单显示

--- health_parent/health_dao/src/main/java/com/itheima/health/dao/UserDao.java
+++ health_parent/health_dao/src/main/java/com/itheima/health/dao/UserDao.java
@@ -1,8 +1,15 @@
 package com.itheima.health.dao;
 
+import com.itheima.health.pojo.Menu;
 import com.itheima.health.pojo.User;
+
+import java.util.List;
 
 public interface UserDao {
 
     User findUserByUsername(String username);
+
+    List<Menu> findMenuListByUserId(Integer id);
+
+    List<Menu> findMenuListLevel2ByMenuId(Integer id);
 }
--- health_parent/health_service/src/main/java/com/itheima/health/service/impl/UserServiceImpl.java
+++ health_parent/health_service/src/main/java/com/itheima/health/service/impl/UserServiceImpl.java
@@ -2,11 +2,14 @@ package com.itheima.health.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.itheima.health.dao.UserDao;
+import com.itheima.health.pojo.Menu;
 import com.itheima.health.pojo.User;
 import com.itheima.health.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -33,7 +36,34 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public List<Map<String, Object>> getMenuList(String username) {
+        User user = userDao.findUserByUsername(username);
 
-        return null;
+        //根据 user.id 查一级菜单列表
+        List<Menu> menuList = userDao.findMenuListByUserId(user.getId());
+
+        List<Map<String,Object>> mapMenu = new ArrayList<>();
+        for (Menu menu : menuList) {
+            Map<String,Object> map = new HashMap<>();
+            map.put("path",menu.getPath());
+            map.put("title",menu.getName());
+            map.put("icon",menu.getIcon());
+            //查二级菜单
+            List<Menu> menuChildrenList = userDao.findMenuListLevel2ByMenuId(menu.getId());
+
+            List<Map<String,Object>> mapMenu1 = new ArrayList<>();
+            if (menuChildrenList != null && menuChildrenList.size() > 0){
+                for (Menu menu1 : menuChildrenList) {
+                    Map<String,Object> map1 = new HashMap<>();
+                    map1.put("path",menu1.getPath());
+                    map1.put("title",menu1.getName());
+                    map1.put("linkUrl",menu1.getLinkUrl());
+                    mapMenu1.add(map1);
+                }
+            }
+            map.put("children",mapMenu1);
+
+            mapMenu.add(map);
+        }
+        return mapMenu;
     }
 }
